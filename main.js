504ccecd7ea60b647e5a7ee669a7c071f5b48742
강양현 <didgusdl0902@gmail.com>
2020-09-29 12:29:50 +0900

    웹서버 추가

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const express=require('express');
+var app=express();
 
-function asdf() {
-    console.log("hello world")
-}
-asdf()
+
+app.get('/', (req, res) => {
+    res.send("안녕하세요");
+});
+app.listen(1024, () => {
+    console.log ("서버가 http://localhost:1024/ 에서 돌아가고 있습니다.");
+});
